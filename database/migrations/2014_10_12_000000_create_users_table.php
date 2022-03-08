fe9fa64efe82c318e99b38332ad0ead4ad4f1898
SafaeQ <safaedani009@gmail.com>
2022-03-08 22:28:35 +0100

    🚀 fixing problems

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_super_admin')->nullable();
+            $table->boolean('is_admin')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
